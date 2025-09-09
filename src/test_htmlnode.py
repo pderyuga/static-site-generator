49f5814dcd3d02a28e61af36cc1a67f1a8f7eee6
pderyuga <polina.deryuga@persuit.com>
2025-09-09 11:12:47 -0400

    feat: add leaf node subclass of html node and tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -36,6 +36,54 @@ class TestHTMLNode(unittest.TestCase):
         node = HTMLNode(props={"href": "https://example.com", "target": "_blank"})
         expected = ' href="https://example.com" target="_blank"'
         self.assertEqual(node.props_to_html(), expected)
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_strong(self):
+        node = LeafNode("strong", "Bold!")
+        self.assertEqual(node.to_html(), "<strong>Bold!</strong>")
+
+    def test_leaf_to_html_em(self):
+        node = LeafNode("em", "Italic text")
+        self.assertEqual(node.to_html(), "<em>Italic text</em>")
+
+    def test_leaf_to_html_code(self):
+        node = LeafNode("code", "x < y && y > 0")
+        self.assertEqual(node.to_html(), "<code>x < y && y > 0</code>")
+
+    def test_leaf_to_html_a_with_props(self):
+        node = LeafNode(
+            "a", "Example", props={"href": "https://example.com", "target": "_blank"}
+        )
+        self.assertEqual(
+            node.to_html(),
+            '<a href="https://example.com" target="_blank">Example</a>',
+        )
+
+    def test_leaf_to_html_span_with_class_and_data_attr(self):
+        node = LeafNode("span", "Badge", props={"class": "badge", "data-kind": "info"})
+        self.assertEqual(
+            node.to_html(),
+            '<span class="badge" data-kind="info">Badge</span>',
+        )
+
+    def test_leaf_to_html_tag_none_returns_value_only(self):
+        node = LeafNode(None, "Just text")
+        self.assertEqual(node.to_html(), "Just text")
+
+    def test_leaf_to_html_raises_when_value_none(self):
+        node = LeafNode("p", None)
+        with self.assertRaises(ValueError) as cm:
+            node.to_html()
+        self.assertEqual(str(cm.exception), "all leaf nodes must have a value")
+
+    def test_leaf_to_html_preserves_whitespace_in_value(self):
+        node = LeafNode("pre", "  indented\nline  ")
+        self.assertEqual(node.to_html(), "<pre>  indented\nline  </pre>")
 
 
 if __name__ == "__main__":
