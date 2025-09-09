29e9f591cea2302b284c9f7db9e10ea7b67d1ae3
pderyuga <polina.deryuga@persuit.com>
2025-09-09 11:39:32 -0400

    feat: add function to split nodes based on specified delimiter and tests

---
+++ src/inline_markdown.py
@@ -0,0 +1,23 @@
+from textnode import TextNode, TextType
+
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+
+        if old_node.text.count(delimiter) % 2 != 0:
+            raise Exception("closing delimeter missing")
+
+        old_node_parts = old_node.text.split(delimiter)
+        for i in range(len(old_node_parts)):
+            if old_node_parts[i] == "":
+                continue
+            if i % 2 == 0:
+                new_nodes.append(TextNode(old_node_parts[i], TextType.TEXT))
+            else:
+                new_nodes.append(TextNode(old_node_parts[i], text_type))
+
+    return new_nodes
---
+++ src/test_inline_markdown.py
@@ -0,0 +1,72 @@
+import unittest
+
+from inline_markdown import split_nodes_delimiter
+from textnode import TextNode, TextType
+
+
+class TestSplitNodesDelimiter(unittest.TestCase):
+    def test_code_basic(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+
+        expected = [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" word", TextType.TEXT),
+        ]
+
+        self.assertEqual(new_nodes, expected)
+
+    def test_bold_basic(self):
+        node = TextNode("before **bold** after", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        expected = [
+            TextNode("before ", TextType.TEXT),
+            TextNode("bold", TextType.BOLD),
+            TextNode(" after", TextType.TEXT),
+        ]
+        self.assertEqual(new_nodes, expected)
+
+    def test_bold_multiple_pairs(self):
+        node = TextNode("a **x** b **y** c", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+        expected = [
+            TextNode("a ", TextType.TEXT),
+            TextNode("x", TextType.BOLD),
+            TextNode(" b ", TextType.TEXT),
+            TextNode("y", TextType.BOLD),
+            TextNode(" c", TextType.TEXT),
+        ]
+        self.assertEqual(new_nodes, expected)
+
+    def test_italic_at_string_edges(self):
+        node = TextNode("_start_ and _end_", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "_", TextType.ITALIC)
+        expected = [
+            TextNode("start", TextType.ITALIC),
+            TextNode(" and ", TextType.TEXT),
+            TextNode("end", TextType.ITALIC),
+        ]
+        self.assertEqual(new_nodes, expected)
+
+    def test_no_delimiter_returns_text_node(self):
+        node = TextNode("plain text", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        expected = [TextNode("plain text", TextType.TEXT)]
+        self.assertEqual(new_nodes, expected)
+
+    def test_odd_delimiter_count_raises_for_backtick(self):
+        node = TextNode("broken `code", TextType.TEXT)  # one backtick only
+        with self.assertRaises(Exception) as cm:
+            split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(str(cm.exception), "closing delimeter missing")
+
+    def test_non_text_nodes_are_passed_through_unchanged(self):
+        link = TextNode("Example", TextType.LINK, url="https://example.com")
+        new_nodes = split_nodes_delimiter([link], "`", TextType.CODE)
+        expected = [link]
+        self.assertEqual(new_nodes, expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
